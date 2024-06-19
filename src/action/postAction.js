66d1aee2691e9b17d269d10da3bd2ad242ab6a69
Soo Jung <comme746@naver.com>
2024-06-19 12:27:33 +0900

    Update Actions

---
+++ src/action/postAction.js
@@ -0,0 +1,52 @@
+import api from "../utils/api";
+import * as types from "../constants/post.constants";
+import { toast } from "react-toastify";
+import { commonUiActions } from "./commonUiAction";
+
+const getPostList = (query) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const getPostDetail = (id) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const createPost = (formData, searchQuery) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const deletePost = (id, searchQuery) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const editPost = (formData, id, searchQuery) => async (dispatch) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+export const postActions = {
+  getPostList,
+  createPost,
+  deletePost,
+  editPost,
+  getPostDetail,
+};
