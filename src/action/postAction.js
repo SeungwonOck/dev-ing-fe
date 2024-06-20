82fb1b3b569500e4ae5b9c61961802439cc5bb8b
Soo Jung <comme746@naver.com>
2024-06-20 18:32:14 +0900

    update post area

--- src/action/postAction.js
+++ src/action/postAction.js
@@ -5,25 +5,48 @@ import { commonUiActions } from "./commonUiAction";
 
 const getPostList = (query) => async (dispatch) => {
     try {
-        
+        dispatch({type: types.POST_GET_REQUEST})
+        const res = await api.get(`/post/all`);
+        if(res.status !== 200) {
+            throw new Error('글을 불러오는데 실패하였습니다.')
+        } else {
+            dispatch({type: types.POST_GET_SUCCESS, payload: res.data.data.allPost})
+        }
     } catch (error) {
-        
+        dispatch({type: types.POST_GET_FAIL, payload: error.message})
+        dispatch(commonUiActions.showToastMessage(error.message, "error"))
     }
 };
 
 const getPostDetail = (id) => async (dispatch) => {
     try {
-        
+        dispatch({type: types.GET_POST_DETAIL_REQUEST})
+        const res = await api.get(`/post/${id}`);
+        if(res.status !== 200) {
+            throw new Error('글을 불러오는데 실패하였습니다.')
+        } else {
+            dispatch({type: types.GET_POST_DETAIL_SUCCESS, payload: res.data.data.post})
+        }
     } catch (error) {
-        
+        dispatch({type: types.GET_POST_DETAIL_FAIL, payload: error.message})
+        dispatch(commonUiActions.showToastMessage(error.message, "error"))
     }
 };
 
-const createPost = (formData, searchQuery) => async (dispatch) => {
+const createPost = (formData, navigate) => async (dispatch) => {
     try {
-        
+        dispatch({type: types.POST_CREATE_REQUEST})
+        const res = await api.post('/post', formData);
+        if(res.status !== 200) {
+            throw new Error('새 글 등록에 실패하였습니다. 다시 시도해주세요.')
+        } else {
+            dispatch({type: types.POST_CREATE_SUCCESS, payload: res.data.data})
+            dispatch(commonUiActions.showToastMessage("새 글이 등록되었습니다.", "success"))
+            navigate(`/post/${res.data.data._id}`)
+        }
     } catch (error) {
-        
+        dispatch({type: types.POST_CREATE_FAIL, payload: error.message})
+        dispatch(commonUiActions.showToastMessage(error.message, "error"))
     }
 };
 
--- src/page/PostWrite.js
+++ src/page/PostWrite.js
@@ -1,8 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Form, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { postActions } from '../action/postAction';
+import '../style/write.style.css';
+import CloudinaryUploadWidget from "../utils/CloudinaryUploadWidget";
+
+const initialFormData = {
+  title: '',
+  content: '',
+  image: '',
+  tags: ''
+};
+
 
 const PostWrite = () => {
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [ formData, setFormData ] = useState({...initialFormData });
+  
+  useEffect(()=>{
+    if(!user) {
+      navigate('/login')
+    } 
+  },[user])
+
+  const createPost = async (e) => {
+    e.preventDefault();
+    dispatch(postActions.createPost(formData, navigate));
+  }
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  }
+
+  const uploadedimage = (url) => {
+    setFormData({ ...formData, image: url })
+  }
+
+
   return (
-    <div>PostWrite</div>
+    <div>
+
+        <Form onSubmit={createPost} className='write-form'>
+          <Form.Group controlId="title" className='write-title'>
+            <Form.Control
+              type="text"
+              placeholder="제목을 입력해주세요"
+              required
+              value={formData.title}
+              onChange={(e) => handleChange(e)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="content" className='write-content'>
+            <Form.Control
+              as="textarea"
+              style={{ height: '500px' }}
+              placeholder="내용을 입력해주세요"
+              required
+              value={formData.content}
+              onChange={(e) => handleChange(e)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="tags" className='write-tag'>
+            <Form.Control
+              type="text"
+              placeholder="태그를 입력해주세요"
+              required
+              value={formData.tags}
+              onChange={(e) => handleChange(e)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3 mt-3 profile-img display-center-center gap-10" controlId="profileImage">
+              <div className='img'>
+                <img id="uploadedimage" src={formData.image} alt="uploadedimage"/>
+              </div>
+              <CloudinaryUploadWidget uploadImage={uploadedimage} />
+          </Form.Group>
+
+          <div className="submit">
+            <button className='green-btn' type="submit">
+              등록
+            </button>
+          </div>
+        </Form>
+
+    </div>
   )
 }
 
