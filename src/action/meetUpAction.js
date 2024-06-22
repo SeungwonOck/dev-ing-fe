ca9fc2c90f4c6e76ac44035127e41a7a165bf2bd
sinhey <sinheyyy@gmail.com>
2024-06-23 04:03:49 +0900

    meetUpList 보여주기

--- src/action/meetUpAction.js
+++ src/action/meetUpAction.js
@@ -5,9 +5,17 @@ import { commonUiActions } from "./commonUiAction";
 
 const getMeetUpList = (query) => async (dispatch) => {
     try {
-
+        dispatch({ type: types.MEETUP_GET_REQUEST })
+        const res = await api.get(`/meetup/all`);
+        console.log("getMeetUpList res", res);
+        if (res.status !== 200) {
+            throw new Error('모임들을 불러오는데 실패하였습니다.')
+        } else {
+            dispatch({ type: types.MEETUP_GET_SUCCESS, payload: res.data.data.allMeetUp })
+        }
     } catch (error) {
-
+        dispatch({ type: types.MEETUP_GET_FAIL, payload: error.message })
+        dispatch(commonUiActions.showToastMessage(error.message, "error"))
     }
 };
 
@@ -23,7 +31,6 @@ const createMeetUp = (formData, navigate) => async (dispatch) => {
     try {
         dispatch({ type: types.MEETUP_CREATE_REQUEST });
         const res = await api.post('/meetup', formData);
-        console.log("createMeetUp res", res);
         if (res.status !== 200) {
             throw new Error('새 모임 등록에 실패하였습니다. 다시 시도해주세요.')
         } else {
---
+++ src/component/MeetUpCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import "../style/home.style.css";
+
+const MeetUpCard = ({ meetUp }) => {
+    const navigate = useNavigate();
+
+    const goToMeetUpDetail = () => {
+        navigate(`/meetUp/${meetUp._id}`);
+    }
+
+    return (
+        <div className='home-meet-up-card' onClick={() => goToMeetUpDetail(meetUp._id)}>
+            <div className='img'><img src={meetUp.image} alt='' /></div>
+            <div className='contents'>
+                <div className='title'>{meetUp.title}</div>
+                <div className='schedule green'>{meetUp.location} · {meetUp.date.date}</div>
+                <div className='small-text'>{meetUp.organizer.userName} · 선착순 {meetUp.currentParticipants}/{meetUp.maxParticipants}</div>
+            </div>
+        </div>
+    )
+}
+
+export default MeetUpCard
