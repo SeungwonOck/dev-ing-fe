e3f3c43658a7ff9b168a0327f7930a69dddc1458
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-22 20:39:41 +1000

    MyPage Profile Design

--- src/component/PostTab.js
+++ src/component/PostTab.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ranImg from "../asset/img/no-image.png"
 
 const PostTab = () => {
   return (
-    <div>
-      Post Tab
+    <div className="post-tab">
+      <img src={ranImg} alt="Random" className="post-image" />
     </div>
   )
 }
