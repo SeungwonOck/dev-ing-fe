cf7c4cd89239d81cce001e8842ee12db9955c3d5
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-06-24 00:16:10 +1000

    Direct to PostDetail Page when the user clicks the post in MyDev Page

--- src/component/PostTab.js
+++ src/component/PostTab.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const PostTab = ({post}) => {
+const PostTab = ({ post }) => {
+    const navigate = useNavigate();
   return (
-    <div className="post-tab">
+    <div className="post-tab" onClick={() => { navigate(`/post/${post._id}`) }}>
       <img src={post.image} alt="Random" className="post-image" />
     </div>
   )
