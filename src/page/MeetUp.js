f5168a242165813289ddf4a0b4336e7665826abe
sinhey <sinheyyy@gmail.com>
2024-06-30 11:01:35 +0900

    meetup category 전체 추가

--- src/page/MeetUp.js
+++ src/page/MeetUp.js
@@ -100,6 +100,7 @@ const MeetUp = () => {
 
             <Dropdown.Menu>
               <Dropdown.Header>카테고리별로 모아보기</Dropdown.Header>
+              <Dropdown.Item onClick={() => getMeetUpListByCategory('전체')}>전체</Dropdown.Item>
               <Dropdown.Item onClick={() => getMeetUpListByCategory('스터디')}>스터디</Dropdown.Item>
               <Dropdown.Item onClick={() => getMeetUpListByCategory('프로젝트')}>프로젝트</Dropdown.Item>
               <Dropdown.Item onClick={() => getMeetUpListByCategory('강의')}>강의</Dropdown.Item>
