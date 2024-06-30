e6dfaa7684fa24e8eab90817cb87f8da0a66d81a
Kalimous <elsword485667@gmail.com>
2024-06-30 16:36:52 +0900

    proxy 수정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 const REACT_APP_BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY
+const REACT_APP_PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND
 
-const socket = io(`${REACT_APP_BACKEND_PROXY}:5001`);
+const socket = io(`${REACT_APP_PROD_BACKEND}:5001`);
+// const socket = io(`${REACT_APP_BACKEND_PROXY}:5001`)
 // const socket = io(`http://localhost:5001`);
 
 export default socket;
