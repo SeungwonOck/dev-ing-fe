1d6cbc9a5f3201875d0ba64b2862ff101d40682b
comme-modee <comme746@naver.com>
2024-07-07 10:21:18 +0900

    update be url

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
-const REACT_APP_BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY
+const REACT_APP_BACKEND_SOCKET = process.env.REACT_APP_BACKEND_SOCKET
 
-// const socket = io(`${REACT_APP_BACKEND_PROXY}:5001`);
-
-// console.log('소켓 주소', REACT_APP_BACKEND_PROXY)
-const socket = io(`http://localhost:5001`);
+const socket = io(REACT_APP_BACKEND_SOCKET);
 
 export default socket;
