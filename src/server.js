e96101531c1a3832e61d9a53a78c46c3642cfc22
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-07-09 17:41:01 +1000

    Change api

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const REACT_APP_BACKEND_SOCKET = process.env.REACT_APP_BACKEND_SOCKET
 
-const socket = io('http://localhost:5001');
-// const socket = io(REACT_APP_BACKEND_SOCKET);
+// const socket = io('http://localhost:5001');
+const socket = io(REACT_APP_BACKEND_SOCKET);
 
-export default socket;
+export default socket;p
