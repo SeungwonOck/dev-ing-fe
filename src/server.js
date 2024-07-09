db5d0d927c34eb58ef95f023d4ff7cbd6812e5c7
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-07-09 17:42:46 +1000

    Remove typo

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ const REACT_APP_BACKEND_SOCKET = process.env.REACT_APP_BACKEND_SOCKET
 // const socket = io('http://localhost:5001');
 const socket = io(REACT_APP_BACKEND_SOCKET);
 
-export default socket;p
+export default socket;
